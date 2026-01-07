395aa8f2004bbed2c4f6d2ff25ffc2c7964d346b
rick <edcrfvlily5@gmail.com>
2026-01-07 11:50:15 +0800

    [add] 調整發牌動畫

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -9,15 +9,21 @@ extends Node2D
 func _ready():
 	slot.connect("stopped", Callable(self, "_on_slot_machine_stopped"))
 
+func monster_draw():
+	monster.play("draw")
+	await get_tree().create_timer(1.8).timeout
+	monster.play("laugh")
+	
 func _on_Roll_button_down():
 	if slot.state == slot.State.OFF:
-		monster.play("draw")
+		monster_draw()
 		slot.start()
 		spin_coin.play()
 	elif slot.state == slot.State.ON:
 		monster.play("laugh")
 		slot.stop()
 		spin_coin.play()
+
 	#if $Roll.text == "Roll":
 		#slot.start()
 		#$Roll.text = "Stop"
@@ -25,7 +31,6 @@ func _on_Roll_button_down():
 		#slot.stop()
 
 func _on_slot_machine_stopped():
-	print("machine_stopped")
 	monster.play("laugh")
 	spin_coin.stop()
 	#$Roll.text = "Roll"
